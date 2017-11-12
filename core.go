a20b3767bd1decedb061dbc98e3271e7905aa87a
Southclaws <southclawjk@gmail.com>
2017-11-12 23:50:04 +0000

    added localhost to cors for testing

--- core.go
+++ core.go
@@ -95,7 +95,7 @@ func (app *App) Start() {
 
 	err := http.ListenAndServe(app.config.Bind, handlers.CORS(
 		handlers.AllowedHeaders([]string{"Cache-Control", "X-File-Name", "X-Requested-With", "X-File-Name", "Content-Type", "Authorization", "Set-Cookie", "Cookie"}),
-		handlers.AllowedOrigins([]string{"https://" + app.config.Domain}),
+		handlers.AllowedOrigins([]string{"https://" + app.config.Domain, "http://localhost:3000"}),
 		handlers.AllowedMethods([]string{"OPTIONS", "GET", "HEAD", "POST", "PUT"}),
 		handlers.AllowCredentials(),
 	)(app.router))
